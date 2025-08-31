99a24ce3485781fe0b2b5ebc99dbe8e7c1e335f4
Brian Shirai <brixen@gmail.com>
2025-08-31 16:44:46 -0700

    Add favicon.

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
       <h1
         className="text-center font-semibold tracking-wide
                    text-[#f4f3ef]
-                   text-3xl md:text-5xl lg:text-6xl
+                   text-3xl md:text-5xl lg:text-8xl
                    [text-shadow:-1px_-1px_0_rgba(255,255,255,0.06),_2px_2px_3px_rgba(0,0,0,0.65)]
                    selection:bg-white/10"
       >
